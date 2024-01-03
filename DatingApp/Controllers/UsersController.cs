ab49f9b9f25f610a148e19b8bb7ecd65d78bda44
Ahmedsalahmohammed22 <ahmedabosalah380@gmail.com>
2024-01-03 21:28:31 +0200

    add DatingApp

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using DatingApp.Data;
+using DatingApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return Ok(users);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            return Ok(user);
+        }
+    }
+}
---
+++ DatingApp/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
