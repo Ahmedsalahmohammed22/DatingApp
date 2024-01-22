094f44972a41dd108a43a28c6fa4103fd786686b
Ahmedsalahmohammed22 <ahmedabosalah380@gmail.com>
2024-01-22 04:05:33 +0200

    add routing , toaster , theme and guard in client project and create shared module

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../services/account.service';
 import { User } from '../models/user';
 import { Observable, of } from 'rxjs';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,22 +14,23 @@ export class NavComponent implements OnInit {
   model:any ={}
   currentUser$ : Observable< User | null> = of(null);
 
-  constructor(private accountService : AccountService) { }
+  constructor(private accountService : AccountService , private router : Router, private toastr :ToastrService) { }
 
   ngOnInit(): void {
       this.currentUser$ = this.accountService.currentUser$
+      console.log(this.currentUser$)
   }
 
   login(){
    this.accountService.login(this.model).subscribe({
-    next :response => {
-      console.log(this.model)
-    },
-    error: error => console.log(error)
+    next :() => this.router.navigateByUrl('/members'),
+    error: error => this.toastr.error(error.error)
    })
   }
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
+
   }
 
 }
